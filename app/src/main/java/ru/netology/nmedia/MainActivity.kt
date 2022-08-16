378cc66690ac862d4269f891a3699951b423adea
Dmitry Vinogradov <dmitrvino@gmail.com>
2022-08-17 00:36:15 +0300

    MVVM added

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -1,51 +1,30 @@
 package ru.netology.nmedia
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import ru.netology.nmedia.databinding.PostBinding
+import androidx.appcompat.app.AppCompatActivity
+import ru.netology.nmedia.databinding.*
+import ru.netology.nmedia.viewModel.PostViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel = PostViewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val binding = PostBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val post = Post(
-            id = 0,
-            author = "Dmitry",
-            content = "Events",
-            published = "06.08.2022"
-        )
 
-        var likes = 999
-        var shares = 1_100_000
-
-        fun checkForK(a: Int): String {
-            if (a > 999_999) {
-                return ((a / 100000)).toString() + "M"
-            } else if (a > 9999) {
-                return ((a / 1000)).toString() + "K"
-            } else if (a > 999) {
-                return "%.1f".format(((a.toFloat() / 1000))) + "K"
-            }
-            return a.toString()
-        }
-
-        binding.likesCounter.text = likes.toString()
 
         binding.likes.setOnClickListener {
-            post.likedByMe = !post.likedByMe
-            val imageResId =
-                if (post.likedByMe) R.drawable.ic_baseline_favorite_224 else R.drawable.ic_baseline_favorite_24
-            binding.likes.setImageResource(imageResId)
-            val likeCounter = if (post.likedByMe) checkForK(++likes) else checkForK(--likes)
-            binding.likesCounter.text = likeCounter
+            binding.likes.setImageResource(viewModel.onLikeClicked())
+            binding.likesCounter.text = viewModel.likeCounter()
         }
 
         binding.share.setOnClickListener {
-            binding.shareCounter.text = checkForK(++shares)
+            binding.shareCounter.text = viewModel.shareCounter()
         }
-
     }
+
 }
---
+++ app/src/main/java/ru/netology/nmedia/viewModel/PostViewNidel.kt
@@ -0,0 +1,19 @@
+package ru.netology.nmedia.viewModel
+
+import androidx.lifecycle.ViewModel
+import ru.netology.nmedia.data.PostRepository
+import ru.netology.nmedia.data.impl.InMemoryPostRepository
+
+class PostViewModel: ViewModel() {
+
+    private val repository : PostRepository = InMemoryPostRepository()
+
+    val data by repository::data
+
+    fun onLikeClicked() = repository.likes()
+
+    fun likeCounter() = repository.likeCounter()
+
+    fun shareCounter() = repository.shareCounter()
+
+}
