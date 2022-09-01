fa7b96562d14cd9729c30b462f9051c9229c593b
Dmitry Vinogradov <dmitrvino@gmail.com>
2022-09-01 21:37:05 +0300

    Intent added

--- app/src/main/java/ru/netology/nmedia/Post.kt
+++ app/src/main/java/ru/netology/nmedia/Post.kt
@@ -8,6 +8,6 @@ data class Post(
     val likes: Int = 0,
     val shares: Int = 0,
     val likedByMe: Boolean = false,
-    val video: String = "https://www.youtube.com/watch?v=iO8FMBWKO3Y"
+    val video: String? = "https://www.youtube.com/watch?v=iO8FMBWKO3Y"
 
 )
